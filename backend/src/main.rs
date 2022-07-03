cfa7b1f92cc0c9a51a14106f02c303f6516cfd9a
chenson2018 <46805207+chenson2018@users.noreply.github.com>
2022-07-03 03:45:31 -0400

    very small bit of rust

---
+++ backend/src/main.rs
@@ -0,0 +1,19 @@
+use actix_web::{App, HttpServer};
+use actix_files::Files;
+
+const HOST: &str = "localhost";
+const PORT: u16 = 8000;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    
+    println!("Starting server at: {}:{}", HOST, PORT);
+
+    HttpServer::new(|| {
+        App::new()
+            .service(Files::new("/", "../frontend/"))
+    })
+    .bind(format!("{}:{}", HOST, PORT)).unwrap()
+    .run()
+    .await
+}
