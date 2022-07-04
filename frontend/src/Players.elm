c49b0a6db62af195666118e7a3f7296df7521e2f
Chris Henson <chrishenson.net@gmail.com>
2022-07-04 14:59:05 -0400

    simpler type signature

--- frontend/src/Players.elm
+++ frontend/src/Players.elm
@@ -17,6 +17,11 @@ playerSettingUp: Player -> Bool
 playerSettingUp player =
    (Array.foldl (+) 0 (Array.map (\c -> if c.show then 1 else 0) player.cards)) < 2
 
+splitArray: Int -> Array a -> (Array a, Array a)
+splitArray n arr = 
+   let front = Array.slice 0 n arr in
+   let back = Array.slice n (Array.length arr) arr in
+       (front,back)
 
 dealHelper: (Array Player, Array Card) -> (Array Player, Array Card)
 dealHelper tup = 
@@ -25,13 +30,7 @@ dealHelper tup =
                let (new_player,new_deck) = splitArray 6 deck in
                (Array.push {cards = new_player, score = 0, lock_flip = False} player_arr, new_deck)
 
-
-deal: Int -> ((Array Player, Array Card) -> (Array Player, Array Card)) -> (Array Player, Array Card) -> (Array Player, Array Card)
+deal: Int -> (a -> a) -> a -> a
 deal i f acc = 
         if i <= 0 then acc else deal (i-1) f (f acc)
 
-splitArray: Int -> Array a -> (Array a, Array a)
-splitArray n arr = 
-   let front = Array.slice 0 n arr in
-   let back = Array.slice n (Array.length arr) arr in
-       (front,back)
